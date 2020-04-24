387e3bcadf65eb828a0ca6a5ba4afcff24dfbe66
Ítalo Médici <italomedice969@gmail.com>
2020-04-23 21:34:34 -0300

    Add exs lista 2

--- Lab_Int_Programação/Lista2_19-04/ex011.cpp
+++ Lab_Int_Programação/Lista2_19-04/ex011.cpp
@@ -1,4 +1,10 @@
-/**/
+/*
+A algum tempo vocês fizeram um exercício que verificava o IMC de uma pessoa e dizia se ela
+estava acima do peso ou não. Refaça este exercício, porém no final você deve perguntar ao usuário:
+    Deseja consultar o IMC de outra pessoa (s/n) ?
+    Caso o usuário digite ‘s’ ou ‘S’ o programa deverá limpar a tela com system(“cls”), e voltar ao início, imprimindo novamente o texto inicial do programa. Caso contrário o programa deverá ser fechado. 
+    Dica: utilize um laço do while.
+*/
 
 #include<stdlib.h>
 #include<stdio.h>
