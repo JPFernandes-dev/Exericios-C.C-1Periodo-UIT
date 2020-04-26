2134189d30558a57167bbd12dbf1d44d1c162ef7
Ítalo Médici <italomedice969@gmail.com>
2020-04-26 14:52:30 -0300

    Update ex010

--- Lab_Int_Programação/Exercícios Resolvidos/Lista2_19-04/ex010.cpp
+++ Lab_Int_Programação/Exercícios Resolvidos/Lista2_19-04/ex010.cpp
@@ -1,5 +1,23 @@
 /* 
     Faça um programa parecido com o anterior, mas agora para imprimir a letra E. Obs.: todos as chamadas da função printf deverão estar dentro do laço for, com exceção dos “\n” finais. Para fazer impressões diferentes em cada iteração, utilize comandos condicionais (if) dentro do laço for.
+    Exemplo de saída:
+        EEEEEEEEEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEEEEEEEEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEE
+        EEEEEEEEEE 
 */
 
 #include<Stdlib.h>
