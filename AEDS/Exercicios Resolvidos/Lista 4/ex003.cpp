cd77994638a2c07bfe79ab453cc278e9f0246c23
Ítalo Médici <italomedice969@gmail.com>
2020-05-03 18:21:45 -0300

    Update ex003

--- AEDS/Exercicios Resolvidos/Lista 4/ex003.cpp
+++ AEDS/Exercicios Resolvidos/Lista 4/ex003.cpp
@@ -6,15 +6,26 @@
 #include<stdio.h>
 
 int main(){
-    int num[12], i;
+    int num[12], i, j, aux_dec;
     // Recebe os valores
     for(i=0;i<12;i++){
         printf("Digite o %d numero: ", i+1);
         scanf("%d", &num[i]);
     }
-    // Mostra o vetor em um laço invertido
-    for(i=12;i>0;i--){
-        printf("%do: %d\n", i, num[i-1]);
+
+    for(i=0;i<12;i++){
+        for(j=i+1;j<12;j++){
+            if(num[i]<num[j]){
+                aux_dec = num[i];
+                num[i] = num[j];
+                num[j] = aux_dec;
+            }
+
+        }
+    } 
+
+    for(i=0;i<12;i++){
+        printf("\n%d", num[i]);
     }
 
     printf("\n\n");
